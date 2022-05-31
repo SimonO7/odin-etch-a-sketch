e51ff1a262ef41c5dcb4b73e9dd710ca9d1e9ee0
SimonO7 <sdragonz461@gmail.com>
2022-05-31 14:12:38 -0700

    Update variable names for more clarity

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,21 +1,21 @@
 const gridContainer = document.querySelector(".grid-container");
 
 //Set the parameters
-let gridSize = 3;
-let borderSize = 2;
-let boxSize = (900/gridSize)-borderSize;
+let numOfSquares = 16;
+let borderSize = 1;
+let squareSize = (700/numOfSquares)- (2*borderSize);    //(total canvas size/number of squares needed) - (sum of left and right borders)
 
-for(let row = 0; row < gridSize; row++) {
+for(let row = 0; row < numOfSquares; row++) {
     //Create a row div
     let rowDiv = document.createElement('div');
     rowDiv.classList.add('row-div');
     
     //Create the squares and put into row div
-    for (let rowSquare = 0; rowSquare < gridSize; rowSquare++) {
+    for (let rowSquare = 0; rowSquare < numOfSquares; rowSquare++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add('grid-square');
-        gridSquare.style.height = `${boxSize}px`
-        gridSquare.style.width = `${boxSize}px`
+        gridSquare.style.height = `${squareSize}px`
+        gridSquare.style.width = `${squareSize}px`
         rowDiv.appendChild(gridSquare);
     }
 
