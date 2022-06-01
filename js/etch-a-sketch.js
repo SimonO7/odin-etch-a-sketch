76e9e811f3521f8f3ce21a82c10944800ff05624
SimonO7 <sdragonz461@gmail.com>
2022-05-31 23:17:40 -0700

    Add documentation for each function

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,6 +1,10 @@
 const gridSizeValue = document.getElementById("gridSizeValue")
 
 function createGrid(numOfSquares) {
+    /**Create a grid with specified number of squares, inside the grid container.
+     * @param   numOfSquares    Number, the number of squares inside the grid.
+     * @returns None
+     */
     const gridContainer = document.querySelector(".grid-container");
     const borderSize = 1;
     const squareWidth = (gridContainer.clientWidth/numOfSquares)-(2*borderSize);    //(total canvas wiidth/number of squares needed) - (sum of left and right borders)
@@ -29,21 +33,38 @@ function createGrid(numOfSquares) {
 }
 
 function addColor(event) {
+    /**Change the color of the square
+     * @param   event   event object, the mouseEnter event object
+     * @returns None
+     */
     event.target.style.backgroundColor = "black";
 }
 
 function clearGrid() {
+    /**Clear the current grid
+     * @param   None
+     * @returns None
+    */
     const rowDivs = document.querySelectorAll(".row-div");
     rowDivs.forEach(rowDiv => rowDiv.remove());
 }
 
 function updateGrid(event) {
+    /**Update the current grid when the grid size value is changed.
+     * @param   event   Event object, the input event.
+     * @returns None
+     */
     clearGrid();
     createGrid(event.target.value);
     gridSizeValue.textContent = event.target.value;
 }
 
 function main() {
+    /**Main function to run at page load. Add event listener to the gridSize slider and create the grid with
+     * default 16 x 16 size.
+     * @param   None
+     * @returns None
+     */
     const gridSize = document.querySelector("#gridSize");
     gridSize.addEventListener("input", updateGrid);
     gridSizeValue.textContent = gridSize.value;
