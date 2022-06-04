cf9fda03fb69d2e2076af0c158b90bfc073bfc53
SimonO7 <sdragonz461@gmail.com>
2022-06-03 23:21:56 -0700

    Add random color function Add function for toggling random color mode

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,5 +1,6 @@
 const gridSizeValue = document.querySelector("#grid-size-value")
 let color = "#000000"
+let randomColorMode = false;
 let linesVisible = true;
 
 function createGrid(numOfSquares) {
@@ -34,11 +35,16 @@ function createGrid(numOfSquares) {
 }
 
 function addColor(event) {
-    /**Change the color of the square
+    /**Change the color of the square. If randomColorMode is on, use a random color. Else, use the selected color.
      * @param   event   event object, the mouseEnter event object
      * @returns None
      */
-    event.target.style.backgroundColor = color;
+    if(randomColorMode) {
+        event.target.style.backgroundColor = randomColor();
+    }
+    else {
+        event.target.style.backgroundColor = color;
+    }
 }
 
 function clearGrid() {
@@ -76,6 +82,32 @@ function toggleGridLines() {
     }
 }
 
+function randomColor() {
+    /**Generate a random RGB color value
+     * @param   None
+     * @returns string, a RGB value.
+     */
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    return (`rgb(${r}, ${g}, ${b})`);
+}
+
+function toggleRandomColor(event) {
+    /**Toggle between random color mode and single color mode. Button will update to show if random color mode
+     *      is on or not.
+     * @param   event   Event object
+     * @returns None
+     */
+    if (randomColorMode) {
+        randomColorMode = false;
+    }
+    else {
+        randomColorMode = true;
+    }
+    event.target.classList.toggle("button-selected")
+}
+
 function main() {
     /**Main function to run at page load. Add event listener to the buttons and create the grid with
      * default 16 x 16 size.
@@ -86,10 +118,14 @@ function main() {
     const clearGridBtn = document.querySelector("#clear-grid-btn");
     const colorPicker = document.querySelector("#color-picker");
     const toggleGridBtn = document.querySelector("#toggle-grid-lines");
+    const randomColorModeBtn = document.querySelector("#random-color-mode");
+
     gridSize.addEventListener("input", (event) => updateGrid(event.target.value));
     clearGridBtn.addEventListener("click", () => updateGrid(gridSize.value));
     toggleGridBtn.addEventListener("click", toggleGridLines);
+    randomColorModeBtn.addEventListener("click", toggleRandomColor);
     colorPicker.addEventListener("input", (event) => color = event.target.value);
+
     gridSizeValue.textContent = gridSize.value;
     createGrid(gridSize.value);
 }
